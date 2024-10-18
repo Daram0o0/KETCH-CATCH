7e56a17772e71610b40c9f7b0abfcf68d760cfce
Daram0o0 <c30728535@gmail.com>
2024-10-18 21:53:33 +0900

    fix: index.ts 경로 문제 해결

--- src/assets/ghost/index.ts
+++ src/assets/ghost/index.ts
@@ -1,10 +1,10 @@
 import Ghost1 from "./ghost1.svg?react";
-import Ghost2 from "./ghost1.svg?react";
-import Ghost3 from "./ghost1.svg?react";
-import Ghost4 from "./ghost1.svg?react";
-import Ghost5 from "./ghost1.svg?react";
-import Ghost6 from "./ghost1.svg?react";
-import Ghost7 from "./ghost1.svg?react";
-import Ghost8 from "./ghost1.svg?react";
+import Ghost2 from "./ghost2.svg?react";
+import Ghost3 from "./ghost3.svg?react";
+import Ghost4 from "./ghost4.svg?react";
+import Ghost5 from "./ghost5.svg?react";
+import Ghost6 from "./ghost6.svg?react";
+import Ghost7 from "./ghost7.svg?react";
+import Ghost8 from "./ghost8.svg?react";
 
 export { Ghost1, Ghost2, Ghost3, Ghost4, Ghost5, Ghost6, Ghost7, Ghost8 };
