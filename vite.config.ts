57400394419bd5232075870a42399c19a294f68a
Daram0o0 <c30728535@gmail.com>
2024-10-18 20:42:19 +0900

    feat: SVG 파일 및 절대경로 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      api: "/src/api",
+      assets: "/src/assets",
+      components: "/src/components",
+      pages: "/src/pages",
+      slices: "/src/slices",
+      store: "/src/store",
+      styles: "/src/styles",
+      types: "/src/tyles",
+      hooks: "/src/hooks",
+      services: "/src/services",
+      utils: "/src/utils",
+    },
+  },
+});
