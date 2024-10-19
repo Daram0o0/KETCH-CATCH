f616913442f450dbca9673d100df17d2bf6b5796
Daram0o0 <c30728535@gmail.com>
2024-10-19 15:11:39 +0900

    feat: web-rankingpage rankingchart 퍼블리싱

--- src/pages/ranking/RankingContainer.tsx
+++ src/pages/ranking/RankingContainer.tsx
@@ -1,7 +1,90 @@
 import RankingUi from "./RankingUi";
 
+const rankingChart = [
+  {
+    rank: 1,
+    id: "Bella",
+    icons: {
+      ghost: "Ghost1",
+      eyes: "Eyes1",
+      color: "yellow",
+    },
+    score: 1230,
+  },
+  {
+    rank: 2,
+    id: "Max",
+    icons: {
+      ghost: "Ghost1",
+      eyes: "Eyes2",
+      color: "gray",
+    },
+    score: 1220,
+  },
+  {
+    rank: 3,
+    id: "Lily",
+    icons: {
+      ghost: "Ghost3",
+      eyes: "Eyes3",
+      color: "pink",
+    },
+    score: 1210,
+  },
+  {
+    rank: 4,
+    id: "John",
+    icons: {
+      ghost: "Ghost4",
+      eyes: "Eyes4",
+      color: "white",
+    },
+    score: 1200,
+  },
+  {
+    rank: 5,
+    id: "Emma",
+    icons: {
+      ghost: "Ghost5",
+      eyes: "Eyes5",
+      color: "lightblue",
+    },
+    score: 1190,
+  },
+  {
+    rank: 6,
+    id: "David",
+    icons: {
+      ghost: "Ghost6",
+      eyes: "Eyes6",
+      color: "lightgreen",
+    },
+    score: 1180,
+  },
+  {
+    rank: 7,
+    id: "Sophia",
+    icons: {
+      ghost: "Ghost7",
+      eyes: "Eyes7",
+      color: "orange",
+    },
+    score: 1170,
+  },
+  {
+    rank: 8,
+    id: "Liam",
+    icons: {
+      ghost: "Ghost8",
+      eyes: "Eyes8",
+      color: "purple",
+    },
+    score: 1160,
+  },
+];
+
 const RankingContainer = () => {
-  return <RankingUi />;
+  return <RankingUi rankingChart={rankingChart} />;
 };
 
 export default RankingContainer;
--- src/pages/ranking/RankingUi.tsx
+++ src/pages/ranking/RankingUi.tsx
@@ -1,5 +1,58 @@
-const RankingUi = () => {
-  return <div></div>;
+import { Ghost1 } from "assets/ghost";
+interface PlayerIcon {
+  ghost: string;
+  eyes: string;
+  color: string;
+}
+
+interface Player {
+  rank: number;
+  id: string;
+  icons: PlayerIcon;
+  score: number;
+}
+
+interface RankingChartProps {
+  rankingChart: Player[];
+}
+
+export const RankCard: React.FC<Player> = ({ rank, id, icons, score }) => {
+  return (
+    <div className="flex justify-between items-center bg-purple-400 rounded-lg shadow-md w-3/4 p-3 m-2">
+      <div className="flex items-center space-x-2">
+        <span className="text-yellow-500 font-bold text-2xl">{rank}</span>
+        <Ghost1 className={`w-5 h-5 ${icons.color}`} />
+        {/* 나중에 캐릭터조합 컴포넌트 만들 예정: 준비물 ghost, eye,color */}
+        <span className="text-white font-bold">{id}</span>
+      </div>
+      <span className="text-white font-bold">{score}</span>
+    </div>
+  );
+};
+
+const RankingUi: React.FC<RankingChartProps> = ({ rankingChart }) => {
+  return (
+    <div className="flex h-screen flex-col">
+      <div className="bg-red-300 h-14">{/* 여긴 로고? 헤더 들어가겠지? */}</div>
+      <main className="flex flex-auto items-center">
+        <div className="w-1/2 bg-blue-300">1,2,3등 표현</div>
+        <div className="w-1/2 bg-gray-400">
+          <div className="w-full h-full flex flex-col items-start">
+            {rankingChart.map((player) => (
+              <RankCard
+                key={player.rank}
+                rank={player.rank}
+                id={player.id}
+                icons={player.icons}
+                score={player.score}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+      <div className="bg-pink-300 h-14">{/* 버튼 컴포넌트 들어 올 곳 */}</div>
+    </div>
+  );
 };
 
 export default RankingUi;
