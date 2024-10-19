9021e2f50ccfd74fea3db4d424b109eb147ec92d
[IoT3] 이다은 <lgc6466@naver.com>
2024-10-20 02:29:24 +0900

    fix: nav border 색 수정

--- src/components/Home/Nav.tsx
+++ src/components/Home/Nav.tsx
@@ -3,7 +3,7 @@ import moon from "assets/mode/moon.svg";
 
 const Home: React.FC = () => {
   return (
-    <header className="flex justify-between items-center p-5">
+    <header className="flex justify-between items-center p-5 max-w-screen-xl mx-auto">
       <div className="text-2xl">:P</div>
       <nav>
         <ul className="flex items-center space-x-8">
@@ -13,7 +13,10 @@ const Home: React.FC = () => {
             <img src={moon} alt="Mode" className="w-8 h-8" />
           </li>
           <li className="flex items-center">
-            <a href="#" className="text-FF9686 border rounded-3xl px-4 py-2">
+            <a
+              href="#"
+              className="text-FF9686 border border-FF9686 rounded-3xl px-4 py-2"
+            >
               APP downloads
             </a>
           </li>
