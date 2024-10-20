efdde8f95c09887acb69b33a3c05d27df477ad4c
Daram0o0 <c30728535@gmail.com>
2024-10-20 15:38:19 +0900

    fix: 로딩페이지 유령컴포넌트 크기 수정

--- src/pages/LoadingUi.tsx
+++ src/pages/LoadingUi.tsx
@@ -5,9 +5,9 @@ const LoadingUi = () => {
     <div className="flex h-screen flex-col bg-main">
       <p className="p-4 text-xl font-bold text-white">{":P"}</p>
       <div className="flex flex-shrink mt-20 flex-col items-center">
-        <GhostMini className="animate-loading" />
+        <GhostMini className="w-24 h-24 drop-shadow-2xl animate-loading" />
       </div>
-      <h1 className="mt-40 text-center text-white text-3xl font-bold">
+      <h1 className="mt-32 text-center text-white text-3xl font-bold">
         {"Loading . . ."}
       </h1>
     </div>
