bc9916549f6e122a7b378f8adb8d005785003d9f
Daram0o0 <c30728535@gmail.com>
2024-10-21 19:24:20 +0900

    feat: 비밀번호 ON/OFF  토글 기능 추가

--- src/components/button/ToggleSwitch.tsx
+++ src/components/button/ToggleSwitch.tsx
@@ -1,23 +1,27 @@
-import { useState } from "react";
-
-const ToggleSwitch = ({ isChecked = false }) => {
-  const [checked, setChecked] = useState(isChecked);
-
-  const handleToggle = () => setChecked((prevChecked) => !prevChecked);
-
+const ToggleSwitch = ({
+  isChecked,
+  onToggle,
+}: {
+  isChecked: boolean;
+  onToggle: () => void;
+}) => {
   return (
     <label className="inline-flex items-center cursor-pointer">
       <input
         type="checkbox"
         className="sr-only peer"
-        checked={checked}
-        onChange={handleToggle}
+        checked={isChecked}
+        onChange={onToggle}
       />
       <div
-        className={`relative w-11 h-6 rounded-full transition-colors ${checked ? "bg-violet-300" : "bg-gray-200"}`}
+        className={`relative w-11 h-6 rounded-full transition-colors ${
+          isChecked ? "bg-violet-300" : "bg-gray-200"
+        }`}
       >
         <span
-          className={`absolute top-[2px] left-[2px] h-5 w-5 bg-white rounded-full transition-transform transform ${checked ? "translate-x-full" : ""} border border-gray-300`}
+          className={`absolute top-[2px] left-[2px] h-5 w-5 bg-white rounded-full transition-transform transform ${
+            isChecked ? "translate-x-full" : ""
+          } border border-gray-300`}
         ></span>
       </div>
     </label>
